bb78d7921a9fa2649178e875ea2174d74494c7c7
wymc <wymc.mail@qq.com>
2024-01-21 17:45:40 +0800

    init 数字论证器

---
+++ src/main/kotlin/com/github/guigumua/App.kt
@@ -0,0 +1,137 @@
+package com.github.guigumua
+
+import java.math.BigInteger
+import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.math.pow
+
+
+val table = TreeMap<Int, Expression>()
+fun main() {
+    println("输入base")
+    val base = readln()
+    runCatching {
+        base.toInt()
+    }.getOrDefault(0).let {
+        if (it <= 0 || it > 1e7) {
+            println("base必须是小于${1e7}的正整数")
+            return
+        }
+    }
+    for (i in base.indices) {
+        backtrace(base, base.length, 0, 0, IntArray(i + 1))
+    }
+    if (!base.startsWith('0')) {
+        buildTable(intArrayOf(base.toInt(), base.toInt()))
+    }
+    while (!Thread.interrupted()) {
+        println("输入数字")
+        val num = readln().toBigInteger()
+        runCatching {
+            val expression = demolish(num)
+            println("${eval(expression)} = $expression")
+        }.onFailure {
+            println("No solution")
+        }
+    }
+}
+
+
+internal fun backtrace(s: String, n: Int, start: Int, index: Int, path: IntArray) {
+    if (index == path.size - 1) {
+        if (s[start].code == '0'.code && (s.length - start) > 1) {
+            return
+        }
+        path[index] = s.subNumber(start, s.length - 1)
+        buildTable(path)
+        return
+    }
+    for (i in start..<s.length - 1) {
+        if (s[start].code == '0'.code && (i - start) > 0) {
+            return
+        }
+        path[index] = s.subNumber(start, i)
+        backtrace(s, n - i, i + 1, index + 1, path)
+    }
+}
+
+fun String.subNumber(start: Int, end: Int): Int {
+    var result = 0
+    for (i in start..end) {
+        result = result * 10 + this[i].code - 48
+    }
+
+    return result
+}
+
+internal fun buildTable(numbers: IntArray) {
+    expressionBacktrace(numbers, 0, numbers.size * 2 - 1, ArrayDeque(numbers.size), 0, table)
+}
+
+val MAX: BigInteger = BigInteger.valueOf(Int.MAX_VALUE.toLong())
+internal fun demolish(num: BigInteger): Expression {
+    if (num > MAX) {
+        val floorEntry = table.floorEntry(Int.MAX_VALUE) ?: throw Exception("No solution")
+        val key = BigInteger.valueOf(floorEntry.key.toLong())
+        val times = num / key
+        val remainder = num % key
+        var expression: Expression = floorEntry.value
+        if (times > BigInteger.ONE) {
+            expression = BinaryExpression(expression, Op.MUL, demolish(times))
+        }
+        if (remainder != BigInteger.ZERO) {
+            expression = if (times > BigInteger.ZERO) {
+                BinaryExpression(expression, Op.ADD, demolish(remainder))
+            } else {
+                demolish(remainder)
+            }
+        }
+        return expression
+    } else {
+        return demolish(num.toInt())
+    }
+}
+
+internal fun demolish(num: Int): Expression {
+    val floorEntry = table.floorEntry(num) ?: throw Exception("No solution")
+    val times = num / floorEntry.key
+    val remainder = num % floorEntry.key
+    var expression: Expression = floorEntry.value
+    if (times > 1) {
+        expression = BinaryExpression(expression, Op.MUL, demolishRemainder(times))
+    }
+    if (remainder != 0) {
+        expression = if (times > 0) {
+            BinaryExpression(expression, Op.ADD, demolishRemainder(remainder))
+        } else {
+            demolishRemainder(remainder)
+        }
+    }
+    return expression
+}
+
+private fun demolishRemainder(remainder: Int): Expression {
+    val lowerEntry = table.floorEntry(remainder) ?: throw Exception("No solution")
+    val expression = lowerEntry.value
+    return if (remainder == lowerEntry.key) {
+        expression
+    } else {
+        BinaryExpression(expression, Op.ADD, demolishRemainder(remainder - lowerEntry.key))
+    }
+}
+
+internal fun eval(expression: Expression): BigInteger {
+    if (expression is Literal) {
+        return expression.value.toBigInteger()
+    }
+    if (expression is BinaryExpression) {
+        return when (expression.op) {
+            Op.ADD -> eval(expression.left) + eval(expression.right)
+            Op.SUB -> eval(expression.left) - eval(expression.right)
+            Op.MUL -> eval(expression.left) * eval(expression.right)
+            Op.DIV -> eval(expression.left) / eval(expression.right)
+            Op.EXP -> eval(expression.left).toDouble().pow(eval(expression.right).toDouble()).toInt().toBigInteger()
+        }
+    }
+    throw Exception("Unknown expression")
+}
---
+++ src/main/kotlin/com/github/guigumua/expression.kt
@@ -0,0 +1,177 @@
+package com.github.guigumua
+
+import com.github.guigumua.Op.*
+import java.util.TreeMap
+import kotlin.math.pow
+
+internal fun expressionBacktrace(
+    numbers: IntArray,
+    numberIndex: Int,
+    n: Int,
+    stack: ArrayDeque<Expression>,
+    index: Int,
+    results: TreeMap<Int, Expression>
+) {
+    if (index == n) {
+        if (stack.last().value <= 0) return
+        results.compute(stack.last().value) { _, v ->
+            if (v == null) {
+                stack.last()
+            } else {
+                if (v.toString().length > stack.last().toString().length) {
+                    stack.last()
+                } else {
+                    v
+                }
+            }
+        }
+        return
+    }
+    if (numberIndex < numbers.size) {
+        stack.add(Literal(numbers[numberIndex]))
+        expressionBacktrace(numbers, numberIndex + 1, n, stack, index + 1, results)
+        stack.removeLast()
+
+        stack.add(Literal(-numbers[numberIndex]))
+        expressionBacktrace(numbers, numberIndex + 1, n, stack, index + 1, results)
+        stack.removeLast()
+    }
+    if (stack.size >= 2) {
+        val right = stack.removeLast()
+        val left = stack.removeLast()
+
+        if (right !is Literal || right.value >= 0) {
+            stack.add(BinaryExpression(left, ADD, right))
+            expressionBacktrace(numbers, numberIndex, n, stack, index + 1, results)
+            stack.removeLast()
+        }
+
+        if (right !is Literal || right.value >= 0) {
+            stack.add(BinaryExpression(left, SUB, right))
+            expressionBacktrace(numbers, numberIndex, n, stack, index + 1, results)
+            stack.removeLast()
+        }
+
+        if (left.value >= 0 || right.value >= 0) {
+            stack.add(BinaryExpression(left, MUL, right))
+            expressionBacktrace(numbers, numberIndex, n, stack, index + 1, results)
+            stack.removeLast()
+        }
+
+        if (right.value != 0 && left.value % right.value == 0) {
+            stack.add(BinaryExpression(left, DIV, right))
+            expressionBacktrace(numbers, numberIndex, n, stack, index + 1, results)
+            stack.removeLast()
+        }
+
+        if (
+            right.value >= 0
+            && (31 - left.value.countLeadingZeroBits() * right.value) <= 31
+            && (31 - left.value.countLeadingZeroBits() * right.value) >= 0
+            && (left.value >= 0 || right.value % 2 != 0)
+        ) {
+            stack.add(BinaryExpression(left, EXP, right))
+            expressionBacktrace(numbers, numberIndex, n, stack, index + 1, results)
+            stack.removeLast()
+        }
+
+        stack.add(left)
+        stack.add(right)
+    }
+}
+
+interface Expression {
+    val precedence: Int
+    val value: Int
+}
+
+internal class Literal(override val value: Int) : Expression {
+    override fun toString(): String {
+        return value.toString()
+    }
+
+    override val precedence: Int
+        get() = Int.MAX_VALUE
+}
+
+internal enum class Op {
+    ADD, SUB, MUL, DIV, EXP;
+
+    override fun toString(): String {
+        return when (this) {
+            ADD -> "+"
+            SUB -> "-"
+            MUL -> "*"
+            DIV -> "/"
+            EXP -> "^"
+        }
+
+    }
+
+}
+
+internal class BinaryExpression(val left: Expression, val op: Op, val right: Expression) : Expression {
+    override val value: Int by lazy(LazyThreadSafetyMode.NONE) { eval() }
+
+    private fun eval(): Int {
+        return when (op) {
+            ADD -> left.value + right.value
+
+            SUB -> left.value - right.value
+
+            MUL -> left.value * right.value
+
+            DIV -> left.value / right.value
+
+            EXP -> left.value.toDouble().pow(right.value.toDouble()).toInt()
+        }
+    }
+
+    private val _string: String by lazy(LazyThreadSafetyMode.NONE) {
+        val sb = StringBuilder()
+        if (left.precedence < precedence || (left.value < 0 && op == EXP)) {
+            sb.append("($left)")
+        } else {
+            sb.append(left)
+        }
+        sb.append(op)
+        when (op) {
+            ADD -> sb.append(right)
+            SUB -> if (right.precedence <= precedence) {
+                sb.append("($right)")
+            } else {
+                sb.append(right)
+            }
+
+            MUL -> if (right.precedence < precedence) {
+                sb.append("($right)")
+            } else {
+                sb.append(right)
+            }
+
+            DIV -> if (right.precedence <= precedence) {
+                sb.append("($right)")
+            } else {
+                sb.append(right)
+            }
+
+            EXP -> if (right.precedence < precedence) {
+                sb.append("($right)")
+            } else {
+                sb.append(right)
+            }
+        }
+        sb.toString()
+    }
+
+    override fun toString(): String {
+        return _string
+    }
+
+    override val precedence: Int
+        get() = when (op) {
+            ADD, SUB -> 1
+            MUL, DIV -> 2
+            EXP -> 3
+        }
+}
